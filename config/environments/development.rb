471739faf3f90eed16230fb6ecba7e8467a5519b
Mike <imiked.mail@gmail.com>
2017-06-04 22:26:01 -0500

    Add  react wizard component

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.x.webpacker[:dev_server_host] = 'http://0.0.0.0:8080'
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
